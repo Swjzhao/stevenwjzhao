b28c36aa6f2a5c9222b658ed861bb4578baacb2b
Steven Zhao <37719422+Swjzhao@users.noreply.github.com>
2021-06-27 23:11:26 -0400

    export app to prep for unit tests

--- server/index.ts
+++ server/index.ts
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
+export default connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Service is running on port ${PORT}`);
   });
