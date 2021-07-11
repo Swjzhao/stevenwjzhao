0b53156fc0efc93c71556648f32c5e5a74b30e48
Steven Zhao <37719422+Swjzhao@users.noreply.github.com>
2021-07-11 18:55:42 -0400

    Change send email

--- server/config/sendEmail.ts
+++ server/config/sendEmail.ts
@@ -4,14 +4,20 @@ const nodemailer = require('nodemailer');
 
 const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground';
 const {
-  MAIL_CLIENT_ID, MAIL_CLIENT_SECRET, MAIL_REFRESH_TOKEN, SENDER_EMAIL_ADDRESS,
+  MAIL_CLIENT_ID,
+  MAIL_CLIENT_SECRET,
+  MAIL_REFRESH_TOKEN,
+  SENDER_EMAIL_ADDRESS,
 } = process.env;
 
 // "Welcome! Please verify your email address."
 
 const sendEmail = async (to: string, url: string, subject: string) => {
-  const oAuth2Client =
-        new OAuth2Client(MAIL_CLIENT_ID, MAIL_CLIENT_SECRET, OAUTH_PLAYGROUND);
+  const oAuth2Client = new OAuth2Client(
+    MAIL_CLIENT_ID,
+    MAIL_CLIENT_SECRET,
+    OAUTH_PLAYGROUND,
+  );
   oAuth2Client.setCredentials({ refresh_token: MAIL_REFRESH_TOKEN });
 
   try {
@@ -29,21 +35,24 @@ const sendEmail = async (to: string, url: string, subject: string) => {
     });
 
     const mailOptions = {
-      from: SENDER_EMAIL_ADDRESS,
+      from: `"We Are Still Dreamers"<${SENDER_EMAIL_ADDRESS}>`,
       to,
       subject,
       html: `
-              <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-              <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to We Are Still Dreamers.</h2>
+            <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
+              <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to WeAreStillDreamers.</h2>
               <p>Congratulations! You're almost set to become a dreamer!
                   Just click the button below to validate your email address.
               </p>
+              <div style="width: 100%; display:flex; justify-content: center;">
               
-              <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">Verify your email address.</a>
-          
+              <a href=${url} style="text-align: center; background: crimson; text-decoration: none; color: white; padding: 10px 20px; display: inline-block;">Verify your email address.</a>
+            </div>
+              
               <p>If the button doesn't work for any reason, you can also click on the link below:</p>
           
-              <div>${url}</div>
+              <div style="max-width: 700px;">
+              <p style="word-break: break-all">${url}</p></div>
               </div>
             `,
     };
