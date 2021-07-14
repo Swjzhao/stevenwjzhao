f11c1398a21d8be2153e52461b06bd80e14fa0ea
Steven Zhao <37719422+Swjzhao@users.noreply.github.com>
2021-07-14 02:09:44 -0400

    Update redis.middleware.ts

--- server/middleware/redis.middleware.ts
+++ server/middleware/redis.middleware.ts
@@ -2,12 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import moment from 'moment';
 import redis from 'redis';
 
-let redisClient: redis.RedisClient;
-if (process.env.REDIS_URL) {
-  redisClient = redis.createClient(process.env.REDIS_URL);
-} else {
-  redisClient = redis.createClient();
-}
+// let redisClient: redis.RedisClient;
+
+const redisClient = redis.createClient({ host: process.env.REDIS_URL });
 
 const WINDOW_SIZE_IN_HOURS = 24;
 const MAX_WINDOW_REQUEST_COUNT = 1000;
