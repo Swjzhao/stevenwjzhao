f0b21d439cd96b2603352496731fc8fe300bb46c
Steven Zhao <37719422+Swjzhao@users.noreply.github.com>
2021-07-14 02:24:05 -0400

    temp remove redis

--- server/index.ts
+++ server/index.ts
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(morgan('dev'));
 app.use(cookieParser());
-app.use(redis);
+// app.use(redis);
 
 app.use('/auth', routes.authRoutes);
 app.use('/user', routes.userRoutes);
