a1dd994ab6ab31e515773aaf3e1ee74f3d845dd0
Steven Zhao <37719422+Swjzhao@users.noreply.github.com>
2021-08-15 21:43:14 -0700

    Update user.action.ts

--- client/src/store/actions/user.action.ts
+++ client/src/store/actions/user.action.ts
@@ -4,16 +4,7 @@ import * as api from '../../api';
 import { IUpdateUser } from '../../interface';
 import { clearStatus, setStatus } from './status.action';
 
-export const resetPassword = (newPassword: string) => async (dispatch: Dispatch) => {
-  try {
-    setStatus({ status: 'loading' });
-    const res = await api.resetPassword(newPassword);
-    clearStatus();
-  } catch (err: any) {
-    setStatus({ status: 'error', error: err.response.data.message });
-  }
-};
-
+/* eslint-disable-next-line */
 export const updateUser = (data: IUpdateUser) => async (dispatch: Dispatch) => {
   try {
     setStatus({ status: 'loading' });
